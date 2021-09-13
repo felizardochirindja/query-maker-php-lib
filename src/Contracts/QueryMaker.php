bff35deec928baa936c2b67be6f224d5b1a754c7
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2021-09-13 13:51:08 -0700

    added update method to QueryMaker interface

--- src/Contracts/QueryMaker.php
+++ src/Contracts/QueryMaker.php
@@ -7,6 +7,6 @@ interface QueryMaker
     function insert(mixed $useId, mixed ...$data) : bool;
     function select(int $firstRecordPosition, int $recordsPerPage) : array;
     function selectOne(int $id) : array;
-    // function update() : bool;
+    function update(array $data) : bool;
     function delete(int $id) : bool;
 }
